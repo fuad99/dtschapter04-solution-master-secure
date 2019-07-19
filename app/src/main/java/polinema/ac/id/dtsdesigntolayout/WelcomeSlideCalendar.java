1ccb8812b4f029bd058d4e6e37f9c975ff492123
hestuning sekar kinanti <hestuningsekarkinanti@hestunings-MacBook-Air.local>
2019-07-19 09:05:00 +0700

    Initial commit

---
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/WelcomeSlideCalendar.java
@@ -0,0 +1,49 @@
+package polinema.ac.id.dtsdesigntolayout;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+public class WelcomeSlideCalendar extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide);
+    }
+
+    public void clickGetStarted(View view) {
+        Intent i = new Intent(WelcomeSlideCalendar.this, WelcomeSlideSuperhero.class);
+        startActivity(i);
+    }
+
+    public void clickLogin(View view) {
+        Intent i = new Intent(WelcomeSlideCalendar.this, WelcomeBack.class);
+        startActivity(i);
+    }
+
+    public void clickContactUs(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"dts@polinema.ac.id"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Test Email");
+        i.putExtra(Intent.EXTRA_TEXT, "Welcome to dts 2019");
+
+        List<ResolveInfo> possibleApps = getPackageManager()
+                .queryIntentActivities(i, PackageManager.MATCH_ALL);
+
+        if (possibleApps.size()>1){
+            String title = "Pilih Email";
+            Intent chooser = Intent.createChooser(i,title);
+            startActivity(chooser);
+        } else if(i.resolveActivity(getPackageManager()) !=null){
+            startActivity(i);
+        }
+    }
+}
